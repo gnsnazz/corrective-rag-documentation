6910755e72eff9b90b9bfbe818de019b1384f54b
nazz <nazimelmadhi2@gmail.com>
2025-12-12 14:38:20 +0100

    tolto variabile inutilizzato

--- app/config.py
+++ app/config.py
@@ -1,8 +1,6 @@
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3.1")
 DB_DIR = "data/vectorstore/transformers_md"
 REPO_PATH = "data/transformers"
